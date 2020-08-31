7d1d22043387516ff7a72d34b795c0f0d0faff1c
OliverBergstrom <70135392+OliverBergstrom@users.noreply.github.com>
2020-08-31 10:36:16 +0200

    skapat variabler, gjort input/outputs

---
+++ Variabler/Variabler.py
@@ -0,0 +1,11 @@
+name = "Oliver" # har skapat variabln name och tilldelat det värdet "Oliver"
+age = 16 # skapat variabeln age och tilldelat det värdet 16
+print(f"Hej {name} du är {age} år gammal")
+
+sida = float(input("Ange kvadratens sida: "))
+
+area = sida**2
+
+omkrets = 4*sida
+
+print(f"kvadratens area är {area} a.e. och kvadratens omkrets är {omkrets} l.e.")
