13edbb88c1830c72a9c746e97e04bc29d3fd27d6
OliverBergstrom <70135392+OliverBergstrom@users.noreply.github.com>
2020-09-02 12:19:29 +0200

    första lektionen

---
+++ Variabler/Räknamedkod.py
@@ -0,0 +1,47 @@
+#1
+print(12-7)
+print(24/3+9)
+print(3*43-2)
+
+#2
+print(f"Hej! hur mår du?")
+
+#3
+Läsk = 15
+print(2*Läsk)
+Hamburgare = 29
+print(2*Läsk+Hamburgare)
+Pommes = 12
+print(3*Läsk+3*Hamburgare+3*Pommes)
+
+#4
+print(219*45*8)
+
+#5
+SPeed = (70*1.5)
+print(f"Om du kör med hastigheten 70 km/h i 1,5 timmar, så hinner du {SPeed} km.")
+
+#6
+print("hej" + "hej")
+print(10*"hej")
+
+#2.1
+bredd = 5
+längd = 8
+
+print("Omkretsen är", 2*(bredd+längd), "cm")
+print("Arean är", (längd*bredd), "kvadratcentimeter")
+
+bredd1 = 92
+längd1 = 230
+
+print("Omkretsen är", 2*(bredd1+längd1), "cm")
+print("Arean är", (längd1*bredd1), "kvadratcentimeter")
+
+bredd2 = 100
+längd2 = 30
+
+print("Omkretsen är", 2*(bredd2+längd2), "cm")
+print("Arean är", (längd2*bredd2), "kvadratcentimeter")
+
+#2
